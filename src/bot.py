1c463fcd52ba537c7bc5580710d042549297502a
Thuan Vo <thuan.vo@ubc.ca>
2021-01-29 13:09:14 -0800

    add noti command

--- src/bot.py
+++ src/bot.py
@@ -9,11 +9,19 @@ class Bot(commands.Bot):
     def __init__(self, command_prefix = '#', description = 'PHYSLAB122_BOT', intents = intents, **options):
         super().__init__(command_prefix = command_prefix, description=description,  **options, intents = intents)
         self.email = "thuan.vo@ubc.ca"
+        self.lab_channels = {
+            "lab03": 796700415293915156,
+            "lab12": 796700478523572234
+        }
 
 
     @commands.command(name = "send_noti", hidden = True)
     @commands.is_owner()
     async def send_notification(self, context, message):
-        pass
+        lab03_channel = discord.utils.get(await message.guild.fetch_channels(), id = self.lab_channels["lab03"])
+        lab12_channel = discord.utils.get(await message.guild.fetch_channels(), id = self.lab_channels["lab12"])
+
+        await lab03_channel.send(message.content)
+        await lab12_channel.send(message.content)
         
 
